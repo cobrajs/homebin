4e2d3f79304973488d3d16751d4794456556961c
cobrajs <cobrasoft@gmail.com>
2013-01-11 13:40:23 -0500

    Added script to show all TODOs in ~/dev

---
+++ devtodo.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+DEVDIR="$HOME/dev"
+
+FILETYPES="hx c h cpp py lua pl sh rb js html css"
+LEAVEOUTS=".git bin Export build DataTables* node_modules"
+
+FILETYPEFIND=""
+for FILETYPE in $FILETYPES; do
+  FILETYPEFIND="$FILETYPEFIND -o -iname \"*.$FILETYPE\""
+done
+
+FILETYPEFIND=$(echo $FILETYPEFIND | cut -d' ' -f 2-)
+
+LEAVEOUTFIND=""
+for LEAVEOUT in $LEAVEOUTS; do
+  LEAVEOUTFIND="$LEAVEOUTFIND ! -wholename \"*/$LEAVEOUT/*\""
+done
+
+FILES=$(eval find $DEVDIR $LEAVEOUTFIND \\\( $FILETYPEFIND \\\) )
+
+grep -s "TODO:" $FILES | sed -e 's/\/\///' -e 's/--//' -e 's/#//' -e 's/<!--//' -e 's/-->//' -e 's/:\s*/\n\t/'
