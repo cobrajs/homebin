c53057ddfadadb14cb4c98850c6c274d6cb0ac84
cobrajs <cobrasoft@gmail.com>
2013-01-17 05:46:16 -0500

    Added tmux config

--- dotfiles/awesome_rc.lua
+++ dotfiles/awesome_rc.lua
@@ -63,14 +63,14 @@ layouts = {
   awful.layout.suit.tile.left,
   awful.layout.suit.tile.bottom,
   awful.layout.suit.tile.top,
-  awful.layout.suit.floating,
+  --awful.layout.suit.floating,
   awful.layout.suit.fair,
   awful.layout.suit.fair.horizontal,
   awful.layout.suit.spiral,
-  awful.layout.suit.spiral.dwindle,
+  --awful.layout.suit.spiral.dwindle,
   awful.layout.suit.max,
   awful.layout.suit.max.fullscreen,
-  awful.layout.suit.magnifier
+  --awful.layout.suit.magnifier
 }
 -- }}}
 
@@ -257,7 +257,7 @@ for s = 1, screen.count() do
     mymemwidget.widget,
     mymemlabel,
     s == 1 and mysystray or nil,
-    --mytasklist[s],
+    mytasklist[s],
     layout = awful.widget.layout.horizontal.rightleft
   }
 end
