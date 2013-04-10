d90f5e280eb5b07d5755ca336eaa5581336af85c
cobrajs <cobrasoft@gmail.com>
2013-04-09 21:14:10 -0400

    Added script for displaying a touchscreen friendly overlay and for generating tmuxinator configs

---
+++ dzen_buttons.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+# Set arg to var so it can be overridden
+ARG1="$1"
+
+# Constants
+FNT="-*-profont-*-*-*-*-22-*-*-*-*-*-*-*"
+HEIGHT=200
+OFFSET=20
+TEXT_WIDTH=11
+
+# Check to see if Mplayer is running
+ACTIVE_WINDOW_ID=$(xprop -root _NET_ACTIVE_WINDOW | cut -d' ' -f5)
+ACTIVE_WINDOW_CLASS=$(xprop -id $ACTIVE_WINDOW_ID WM_CLASS)
+
+if (( $(echo $ACTIVE_WINDOW_CLASS | grep "mplayer" -i -c) > 0 )); then
+  ARG1="mplayer"
+fi
+
+case "$ARG1" in
+  cmus)
+    CONFIG="PREV:cmus-remote -r\nPAUSE:cmus-remote -u\nNEXT:cmus-remote -n\n"
+    ;;
+  mplayer)
+    CONFIG="PAUSE:xdotool key --window $ACTIVE_WINDOW_ID 'space'\nKeyboard:^exit 27\n"
+    ;;
+  *)
+    CONFIG="CMUS:^exit 28\nKeyboard:^exit 27\n"
+    ;;
+esac 
+
+CONFIG="${CONFIG}Exit:^exit 2"
+
+BUTTON_COUNT=$(echo -e $CONFIG | wc -l)
+
+# Calculations
+SCREEN_SIZE=$(xprop -root _NET_DESKTOP_GEOMETRY | cut -d'=' -f2)
+SCREEN_WIDTH=$(echo $SCREEN_SIZE | cut -d',' -f1)
+SCREEN_HEIGHT=$(echo $SCREEN_SIZE | cut -d',' -f2)
+BUTTON_HEIGHT=$(( $HEIGHT - $OFFSET * 2 ))
+BUTTON_WIDTH=$(( $SCREEN_WIDTH / $BUTTON_COUNT - $OFFSET ))
+HALF_WIDTH=$(( $BUTTON_WIDTH / 2 ))
+
+FINAL=""
+
+I=0
+
+while read BUTTON_LINE; do
+#for (( i=0; i<$BUTTON_COUNT; i++));  do
+  if (( $I != 0 )); then 
+    FINAL="$FINAL^p($OFFSET)"
+  fi
+
+  TEXT=$(echo $BUTTON_LINE | cut -d':' -f1)
+  ACTION=$(echo $BUTTON_LINE | cut -d':' -f2)
+
+  LEN=$(echo $TEXT | wc -c)
+  HALF_TEXT_WIDTH=$(( ( $LEN * $TEXT_WIDTH ) / 2 ))
+  TEXT_OFFSET=$(( $HALF_WIDTH + $HALF_TEXT_WIDTH ))
+  RESET=$(( $HALF_WIDTH - $HALF_TEXT_WIDTH ))
+  FINAL="$FINAL^ca(1,$ACTION)^ro(${BUTTON_WIDTH}x${BUTTON_HEIGHT})^ib(1)^p(-$TEXT_OFFSET)$TEXT^p($RESET)^ca()"
+  I=$(( $I + 1 ))
+done < <(echo -e $CONFIG)
+
+echo $FINAL |
+/home/cobra/dev/git/forked/dzen/dzen2 -fn $FNT -h 200 -y $(( $SCREEN_HEIGHT - $HEIGHT)) -p 0 -e "button3=exit:2"
+
+case $? in
+  27)
+    onboard &
+    ;;
+  28)
+    $0 cmus &
+    ;;
+esac
+
---
+++ tmuxinator_gen.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+session_name=
+session_pre=
+if [[ "$1" ]]; then
+  session_name="-t $1"
+  session_pre="$1"
+fi
+
+session_path=$(tmux list-panes -t $session_pre:1 -F "#{pane_start_path}")
+
+echo "project_name:" $(tmux display-message $session_name -p -F "#{session_name}")
+echo "project_root: $session_path" 
+echo "tabs:"
+
+function get_command() {
+  processes=$(ps h -o cmd -s $1 | grep -v "\-bash" | head -n1)
+  if [[ $(expr "$processes" : ".*$0") -gt 0 ]]; then
+    echo -n ""
+  else
+    echo -n $processes
+  fi
+}
+
+while read window; do
+  window_id=$(echo $window | cut -d: -f1)
+  window_name=$(echo $window | cut -d: -f2)
+  window_panes=$(echo $window | cut -d: -f3)
+  window_layout=$(echo $window | cut -d: -f4)
+  
+  if [[ $window_panes -gt 1 ]]; then
+    echo "  - $window_name:"
+    echo "      layout: $window_layout"
+    echo "      panes: "
+  else
+    echo -n "  - $window_name: "
+  fi
+
+  while read pane; do
+    pane_pid=$(echo $pane | cut -d: -f1)
+    pane_current_path=$(echo $pane | cut -d: -f2)
+
+    if [[ $window_panes -gt 1 ]]; then
+      echo -n "        - "
+    fi
+
+    if [[ "$pane_current_path" != "$session_path" ]]; then
+      echo -n "cd $pane_current_path "
+    fi
+
+    comm=$(get_command $pane_pid)
+
+    if [[ "$comm" = "" ]]; then
+      echo
+    else
+      if [[ "$pane_current_path" != "$session_path" ]]; then
+        echo -n "&& "
+      fi
+      echo "$comm"
+    fi
+
+    #echo $pane
+  done < <(tmux list-panes -t $session_pre:$window_id -F "#{pane_pid}:#{pane_current_path}")
+done < <(tmux list-windows $session_name -F "#{window_id}:#{window_name}:#{window_panes}:#{window_layout}")
+
